2bd9b936500f9b1910595da334dec7968fbf1050
Leah W. Chang <leahwchang@gmail.com>
2017-03-06 15:06:00 -0400

    Math.random logic to get random quote

--- public/javascripts/javascripts.js
+++ public/javascripts/javascripts.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
-  $("#getMessage").on("click", function() {
-    // create empty array of quotes
-    var quotes = [];
+  $("#getQuote").on("click", function() {
     // get json from website
     $.getJSON("http://quotes.stormconsultancy.co.uk/quotes.json", function(json) {
+      // create empty array of quotes
+      var quotes = [];
       json.forEach(function(val) {
         quotes.push("<p>" + val.quote + " <br> - " + val.author + "</p>");
       });
-      console.log(quotes[0]);
-      $(".message").html(quotes[0]);
+      // create randomNumber to get quotes[i]
+      var randomNumber = Math.floor(Math.random() * (quotes.length));
+      $(".display").html(quotes[randomNumber]);
     });
   });
 });
